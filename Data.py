a456a5a48aa887431de043bc39f0bb396ea89796
Ron Tena <arontena77@gmail.com>
2026-01-20 00:01:46 +0800

    First commit

---
+++ Data.py
@@ -0,0 +1,52 @@
+import os
+import pandas as pd
+
+def dataCleaning():
+    df = pd.read_csv(r"C:\Users\Tena\Documents\Python proj\Data Cleaning\Customer Call List.csv")
+
+    df = df.drop_duplicates()
+
+    df = df.drop(columns="Not_Useful_Column")
+
+    df["Last_Name"] = df["Last_Name"].str.strip("123._/")
+
+    df["Phone_Number"] = df["Phone_Number"].str.replace('[^a-zA-Z0-9]', "")
+
+    df["Phone_Number"] = df["Phone_Number"].apply(lambda x: str(x))
+
+    df["Phone_Number"] = df["Phone_Number"].apply(lambda x: x[0:3] + '-' + x[3:6] + '-' + x[6:10])
+
+    df["Phone_Number"] = df["Phone_Number"].str.replace("nan--", "")
+
+    df["Phone_Number"] = df["Phone_Number"].str.replace("Na--", "")
+
+    df[["Street_Address", "State", "Zip_Code"]] = df["Address"].str.split(',',2, expand=True) 
+
+    df["Paying Customer"] = df["Paying Customer"].str.replace("Yes", "Y")
+
+    df["Paying Customer"] = df["Paying Customer"].str.replace("No", "N")
+
+    df["Do_Not_Contact"] = df["Do_Not_Contact"].str.replace("Yes", "Y")
+
+    df["Do_Not_Contact"] = df["Do_Not_Contact"].str.replace("No", "N")
+
+    df = df.replace("N/a", "")
+
+    df = df.replace("NaN", "")
+
+    df = df.fillna("")
+
+    for x in df.index:
+        if df.loc[x, "Do_Not_Contact"] == "Y":
+            df.drop(x, inplace=True)
+        
+    for x in df.index:
+        if df.loc[x, "Phone_Number"] == "":
+            df.drop(x, inplace=True)
+
+    df = df.reset_index(drop=True)
+
+    
+
+dataCleaning()
+
